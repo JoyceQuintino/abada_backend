faf08bcb3bf07f1803662bd8051e8ee6c8fa18bf
Paulo Henrique <phsilva098@gmail.com>
2023-08-18 14:17:14 -0300

    addins packages

--- script.py
+++ script.py
@@ -3,4 +3,5 @@ import pandas as pd
 competidores = pd.read_csv('database-abada.csv')
 data_frame = pd.DataFrame(competidores)
 for row in data_frame.itertuples():
+    ''' Criar os objetos usando o cada linha e inserir diretamente no banco de dados '''
     print(row.graduacao)
