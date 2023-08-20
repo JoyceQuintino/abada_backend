66a1ca15919c992b58a4d2335afe725b4bac9805
joyce.alves <joycequintino11@gmail.com>
2023-08-20 16:02:34 -0300

    feat: read csv for dataframe

--- src/database/init_db.py
+++ src/database/init_db.py
@@ -1,15 +1,24 @@
+import pandas as pd
 from asyncio import run
-
 from src.database.db_connection import DBConnection
 from src.models.models import Base
 
 db_conn = DBConnection()
 engine = db_conn.get_engine()
 
+def insert_data() -> pd.DataFrame:
+    data = pd.read_csv('database-abada.csv', sep=',')
+    return data
+
 async def create_database():
     async with engine.begin() as connection:
         await connection.run_sync(Base.metadata.drop_all)
         await connection.run_sync(Base.metadata.create_all)
 
+async def insert_data():
+    async with engine.begin() as connection:
+        await connection.run_sync(Base.metadata.drop_all)
+
 if __name__ == '__main__':
+    #insert_data()
     run(create_database())
