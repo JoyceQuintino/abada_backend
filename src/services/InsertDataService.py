9c762584db51ef57f2ededab092e665f0b1c35b0
Paulo Henrique <phsilva098@gmail.com>
2023-09-07 20:10:16 -0300

    hotfix: insert_data function refactor

--- src/services/InsertDataService.py
+++ src/services/InsertDataService.py
@@ -14,27 +14,10 @@ class InsertDataService:
         async_session = DBConnection().get_engine()
         async with async_session() as session:
             for row in data.itertuples():
-                if row.graduacao in CategoriaEnum.laranja_laranja_azul.__str__():
-                    graduacao = Graduacao(nome=row.graduacao, categoria=CategoriaEnum.laranja_laranja_azul)
-                    session.add(graduacao)
-                elif row.graduacao in CategoriaEnum.azul_azul_verde.__str__():
-                    graduacao = Graduacao(nome=row.graduacao, categoria=CategoriaEnum.azul_azul_verde)
-                    session.add(graduacao)
-                elif row.graduacao in CategoriaEnum.verde_verde_roxa.__str__():
-                    graduacao = Graduacao(nome=row.graduacao, categoria=CategoriaEnum.verde_verde_roxa)
-                    session.add(graduacao)
-                elif row.graduacao in CategoriaEnum.roxa_roxa_marrom.__str__():
-                    graduacao = Graduacao(nome=row.graduacao, categoria=CategoriaEnum.roxa_roxa_marrom)
-                    session.add(graduacao)
-                elif row.graduacao in CategoriaEnum.marrom_marrom_vermelha.__str__():
-                    graduacao = Graduacao(nome=row.graduacao, categoria=CategoriaEnum.marrom_marrom_vermelha)
-                    session.add(graduacao)
-                filiacao = Filiacao(nome_professor=row.filiacao, graduacao_professor='teste')
                 competidor = Competidor(
                     nome=row.nome,
                     apelido=row.apelido,
                     nome_estado=f'{row.cidade}-{row.estado}')
-                session.add(filiacao)
                 session.add(competidor)
                 await session.commit()
             
