e635108d9418ca2adfd00e42050ccb3f80a64cff
joyce.alves <joycequintino11@gmail.com>
2023-09-10 15:01:50 -0300

    modified

--- src/database/init_db.py
+++ src/database/init_db.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from asyncio import run
 from src.database.db_connection import DBConnection
 from src.models.models import Base
@@ -6,19 +5,10 @@ from src.models.models import Base
 db_conn = DBConnection()
 engine = db_conn.get_engine()
 
-def convert_data() -> pd.DataFrame:
-    data = pd.read_csv('database-abada.csv', sep=',')
-    return data
-
 async def create_database():
     async with engine.begin() as connection:
         await connection.run_sync(Base.metadata.drop_all)
         await connection.run_sync(Base.metadata.create_all)
 
-async def insert_data():
-    async with engine.begin() as connection:
-        await connection.run_sync(Base.metadata.drop_all)
-
 if __name__ == '__main__':
-    #convert_data()
     run(create_database())
