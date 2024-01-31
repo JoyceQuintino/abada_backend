f7ab3a718c6cc9b0c364417665be3c2421c0a4e3
Paulo Henrique <phsilva098@gmail.com>
2024-01-31 10:47:40 -0300

    refactor: removendo schema por erro no swagger

--- src/schemas/ChaveamentoSchema.py
+++ src/schemas/ChaveamentoSchema.py
@@ -7,12 +7,3 @@ from dataclasses import dataclass
 class ChaveamentoInput(BaseModel):
     categoria: str = Field(title="Categoria",
                            description="Categoria que será feita o chaveamento")
-
-
-class ChaveamentoOutput(BaseModel):
-    model_config = ConfigDict(arbitrary_types_allowed=True)
-
-    categoria: str = Field(title="Categoria", description="Categoria que ocorreu o chaveamento")
-    jogos_fem: List[List[Jogos]] = Field(title="Jogos fem", description="Lista de jogos femininos")
-    jogos_masc: List[List[Jogos]] = Field(title="Jogos masc", description="Lista de jogos masculinos")
-    competidores_categoria: List[Competidores] = Field(title="Competidores", description="Lista de competidores da categoria")
