4ac4bb09aa94dbc91f9f7a328e31a4331b0eba2a
Paulo Henrique <phsilva098@gmail.com>
2024-05-06 15:16:54 -0300

    refactor: refatorado funcao de inserir dados

--- src/services/InsertDataService.py
+++ src/services/InsertDataService.py
@@ -8,7 +8,7 @@ from sqlalchemy.future import select
 from sqlalchemy.orm import lazyload
 
 def get_data_to_insert():
-        competidores = pd.read_csv('database-teste.csv')
+        competidores = pd.read_csv('dados_falsos.csv')
         data_frame = pd.DataFrame(competidores)
         return data_frame
 
@@ -49,16 +49,6 @@ class InsertDataService:
     async def inserting_data():
         data = get_data_to_insert()
         async with async_session() as session:
-            # result = (await session.scalars(select(Graduacoes))).all()
-            # print(result)
-            # aux = result.scalars().all()
-            # for element in aux:
-            #     print(element.nome)
-            # graduacoes = []
-            # for element in aux:
-            #     graduacoes.append(element)
-            # result = await session.execute(select(Filiacoes))
-            # filiacoes = result.scalars().all()
             result = await session.execute(select(Graduacoes).options(lazyload(Graduacoes.categoria)))
             graduacoes = []
             competidores = []
@@ -68,9 +58,7 @@ class InsertDataService:
                         graduacoes.append(item)
             for graduacao in graduacoes:
                 for row in data.itertuples():
-                    # print(graduacao)
-                    # print(row.graduacao)
-                    if row.graduacao == graduacao.nome.lower():
+                    if row.graduacao.lower() == graduacao.nome:
                         competidor = Competidores(
                             nome=row.nome,
                             apelido=row.apelido,
@@ -81,26 +69,7 @@ class InsertDataService:
                             id_graduacao=graduacao.id
                         )
                         competidores.append(competidor)
+            # print(competidores)
             session.add_all(competidores)
             await session.commit()
-                    # if row.graduacao == graduacao.nome.lower():
-                    #     print(f'{graduacao.nome} + {row.graducao}')
-
-                # for row in data.itertuples():
-                #     aux = Graduacoes()
-                #     if row.graduacao == graduacao.nome.lower():
-                #         print(f'{graduacao} = {row.graduacao}')
-                #         aux = graduacao
-                #         print(aux)
-                # competidor = Competidores(
-                #     nome=row.nome,
-                #     apelido=row.apelido,
-                #     numero=row.numero,
-                #     cidade=row.cidade,
-                #     estado=row.estado,
-                #     sexo=row.sexo,
-                #     id_graduacao=aux.id
-                # )
-                # session.add(competidor)
-                # await session.commit()
             
